dcb5034edbc72b2b83a37f57a7d4a6e1c15f924a
tetsuo shima <primaveraentalca@gmail.com>
2020-06-01 16:58:12 -0400

    no lodash modules

--- src/index.js
+++ src/index.js
@@ -1,60 +1,44 @@
-const random = require("lodash.random");
-const times = require("lodash.times");
-const getSheet = require("./getSheet");
+const { google } = require("googleapis");
 
-module.exports = async (spreadsheetId, range, howMany) => {
+const getSheet = async (spreadsheetId, range) => {
+  try {
+    const auth = await google.auth.getClient({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(
+          /\\n/g,
+          "\n"
+        ).trim(),
+      },
+      scopes: "https://www.googleapis.com/auth/spreadsheets.readonly",
+    });
+
+    const { spreadsheets } = google.sheets({ version: "v4", auth });
+    const { data } = await spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    });
+    return data.values;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const sheetPoetry = async (spreadsheetId, range, howMany) => {
   const values = await getSheet(spreadsheetId, range);
-
-  const [rowRangeMin, rowRangeMax] = range
-    .replace(/\D/g, " ")
-    .split(" ")
-    .filter(el => {
-      return el !== "";
-    })
-    .map(el => {
-      return parseInt(el) - 1;
-    });
-
-  const steps =
-    range
-      .replace(/[^a-z]/gi, "")
-      .split("")
-      .map(el => {
-        return el.charCodeAt(0);
-      })
-      .reduce((acc, num) => {
-        return num - acc;
-      }) + 1;
-
-  const makeVerse = () => {
-    let verse = "";
-    let row;
-    const selectRandomRow = () => {
-      row = values[random(rowRangeMin, rowRangeMax)];
-      if (!row) selectRandomRow();
-    };
-    const selectRandomCell = column => {
-      selectRandomRow();
-      const cell = row[column];
-      if (cell !== undefined) {
-        verse += ` ${cell.trim()}`;
-      } else {
-        selectRandomCell(column);
-      }
-    };
-    times(steps, step => selectRandomCell(step));
-    return verse.trim();
-  };
-
-  const makePoem = () => {
-    let poem = "";
-    times(howMany, step => {
-      poem += step == howMany - 1 ? `${makeVerse()}` : `${makeVerse()}\n`;
-    });
+  const randomRow = () => values[Math.floor(Math.random() * values.length)];
+  const sheetVerse = () => randomRow().map((cell, index) => randomRow()[index]);
+  const sheetPoem = (howMany) => {
+    let poem = [];
+    for (let i = 0; i < howMany; i++) {
+      poem.push(sheetVerse().join(" "));
+    }
     return poem;
   };
+  return sheetPoem(howMany).join("\n");
+};
 
-  const sheetpoem = makePoem();
-
-  return sheetpoem;
+module.exports = {
+  getSheet,
+  sheetPoetry,
 };
